b346be7d4fd916422edd231c4dad74b4e94ab457
hasanssahin <sahinhhasann@gmail.com>
2023-07-25 22:10:53 +0300

    required

---
+++ src/main/java/com/hasansahin/airlineticketingsystem/AirlineTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hasansahin.airlineticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketingSystemApplication.class, args);
+	}
+
+}
