a0f265b879885e4748e75406467e6646b0facb0c
hasanssahin <sahinhhasann@gmail.com>
2023-07-25 22:12:53 +0300

    Create Route entity

---
+++ src/main/java/com/hasansahin/airlineticketingsystem/model/Route.java
@@ -0,0 +1,33 @@
+package com.hasansahin.airlineticketingsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Route {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String routeName;
+	private String uuid;
+
+	@OneToOne
+	@JoinColumn(name = "departureAirportId")
+	private Airport departureAirport;
+
+	@OneToOne
+	@JoinColumn(name = "arrivalAirportId")
+	private Airport arrivalAirport;
+
+	public Route(String routeName) {
+		this.routeName = routeName;
+		this.uuid= UUID.randomUUID().toString();
+	}
+}
