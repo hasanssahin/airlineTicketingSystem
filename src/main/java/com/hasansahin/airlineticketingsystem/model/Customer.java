0185aeff9a3496c032f4d58f8298ae6e75c4f57c
hasanssahin <sahinhhasann@gmail.com>
2023-07-25 22:12:32 +0300

    Create Customer entity

---
+++ src/main/java/com/hasansahin/airlineticketingsystem/model/Customer.java
@@ -0,0 +1,29 @@
+package com.hasansahin.airlineticketingsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String surname;
+	private String email;
+
+	public Customer(String name, String surname, String email) {
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+}
