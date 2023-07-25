4893cc9818f34bf9d142166abf4d7ae121f5d3a3
hasanssahin <sahinhhasann@gmail.com>
2023-07-25 22:12:24 +0300

    Create CreditCard entity

---
+++ src/main/java/com/hasansahin/airlineticketingsystem/model/CreditCard.java
@@ -0,0 +1,26 @@
+package com.hasansahin.airlineticketingsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class CreditCard {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String creditCardNumber;
+	private String cvc;
+
+	public CreditCard(String creditCardNumber, String cvc) {
+		this.creditCardNumber = creditCardNumber;
+		this.cvc = cvc;
+	}
+}
