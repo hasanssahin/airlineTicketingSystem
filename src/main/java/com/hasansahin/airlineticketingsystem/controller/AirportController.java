297b68f0e5cc624f75691e52fbbe63faef380134
hasanssahin <sahinhhasann@gmail.com>
2023-07-26 12:16:07 +0300

    Add find method

--- src/main/java/com/hasansahin/airlineticketingsystem/controller/AirportController.java
+++ src/main/java/com/hasansahin/airlineticketingsystem/controller/AirportController.java
@@ -4,10 +4,7 @@ import com.hasansahin.airlineticketingsystem.dto.AirportDto;
 import com.hasansahin.airlineticketingsystem.service.AirportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +15,10 @@ public class AirportController {
 	@PostMapping
 	public ResponseEntity<AirportDto> save(@RequestBody AirportDto airportDto) {
 		return ResponseEntity.ok(airportService.save(airportDto));
+	}
+
+	@GetMapping
+	public ResponseEntity<AirportDto> findByIataCode(String iataCode) {
+		return ResponseEntity.ok(airportService.findByIataCode(iataCode));
 	}
 }
