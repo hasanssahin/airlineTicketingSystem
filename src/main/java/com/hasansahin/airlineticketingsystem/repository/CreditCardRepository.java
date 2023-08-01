375e0f844cd8709a804f3056fcc077b9b23aa3d3
hasanssahin <sahinhhasann@gmail.com>
2023-08-01 18:40:08 +0300

    Add optional

--- src/main/java/com/hasansahin/airlineticketingsystem/repository/CreditCardRepository.java
+++ src/main/java/com/hasansahin/airlineticketingsystem/repository/CreditCardRepository.java
@@ -3,6 +3,8 @@ package com.hasansahin.airlineticketingsystem.repository;
 import com.hasansahin.airlineticketingsystem.model.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
-    CreditCard findByCvc(String cvc);
+    Optional<CreditCard> findByCvc(String cvc);
 }
